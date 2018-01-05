9706c36b7d0d8790079acfca4f5e08014c9d49cb
Avi <avrahams1@gmail.com>
2018-01-05 14:07:52 +0200

    Eject Webpakc settings out, add CSS imports

--- src/components/About/About.js
+++ src/components/About/About.js
@@ -2,14 +2,13 @@
 import React, { Component } from 'react';
 import classnames from 'classnames';
 
-import './style.css';
+import styles from './style.css';
 
 export default class About extends Component {
   render() {
-    const { className, ...props } = this.props;
     return (
-      <div className={classnames('About', className)} {...props}>
-        <h1>
+      <div className={classnames('About')}>
+        <h1 className={classnames(styles.bla, styles.bla2)}>
           React Router working!
         </h1>
       </div>
--- src/components/App/App.js
+++ src/components/App/App.js
@@ -2,24 +2,24 @@ import React, { Component } from 'react';
 import classnames from 'classnames';
 import { Link } from 'react-router-dom';
 import logo from './logo.svg';
-import './style.css';
+import styles from './style.css';
 
 class App extends Component {
 
   render() {
-    const { className, ...props } = this.props;
     return (
-      <div className={classnames('App', className)} {...props}>
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
+      <div className={styles.App}>
+        <div className={styles['App-header']}>
+          <img src={logo} className={styles['App-logo']} alt="logo" />
           <h2>Welcome to React - Fullstack!</h2>
         </div>
-        <p className="App-intro">
+        <p className={styles["App-intro"]}>
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
         <Link to='about'><button>Test React Router</button></Link>
         <br />
         <br />
+        <h1>test</h1>
         <button onClick={this.props.actions.expressTest}>Test if Express is working</button>
         <br />
         <br />
